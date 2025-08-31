32759fbfb62020211cdbcbff39fba2eb0c2dd99d
Amirhossein Fazli <amirhossein95b@gmail.com>
2025-08-31 12:15:29 +0330

    Add validate method in `PasswordGeneratorOptions`

--- internal/password_generator_options.go
+++ internal/password_generator_options.go
@@ -1,29 +1,44 @@
 package internal
 
+import "errors"
+
+var (
+	ErrLengthMustBeGreaterThanZero              = errors.New("Length must be greater than 0.")
+	ErrAvoidRepeatsMustBeEqualOrGreaterThanZero = errors.New("Avoid repeats must be greater than or equal to 0.")
+)
+
 type PasswordGeneratorOptions struct {
-	Length         int
-	Lowercase      bool
-	Uppercase      bool
-	Numbers        bool
-	Symbols        bool
-	Exclude        string
-	ExcludeSimilar bool
-	Custom         string
-	AvoidRepeats   int
-	QrCode         bool
+	Length       int
+	Lowercase    bool
+	Uppercase    bool
+	Numbers      bool
+	Symbols      bool
+	Custom       string
+	AvoidRepeats int
+	QrCode       bool
 }
 
 func NewPasswordGeneratorOptions() *PasswordGeneratorOptions {
 	return &PasswordGeneratorOptions{
-		Length:         12,
-		Lowercase:      true,
-		Uppercase:      true,
-		Numbers:        true,
-		Symbols:        false,
-		Exclude:        "",
-		ExcludeSimilar: false,
-		Custom:         "",
-		AvoidRepeats:   1,
-		QrCode:         false,
+		Length:       12,
+		Lowercase:    true,
+		Uppercase:    true,
+		Numbers:      true,
+		Symbols:      false,
+		Custom:       "",
+		AvoidRepeats: 1,
+		QrCode:       false,
 	}
+}
+
+func (p *PasswordGeneratorOptions) Validate() (bool, error) {
+	if p.Length <= 0 {
+		return false, ErrLengthMustBeGreaterThanZero
+	}
+
+	if p.AvoidRepeats < 0 {
+		return false, ErrAvoidRepeatsMustBeEqualOrGreaterThanZero
+	}
+
+	return true, nil
 }
--- internal/password_generator_options_test.go
+++ internal/password_generator_options_test.go
@@ -14,9 +14,86 @@ func TestPasswordGeneratorOptions(t *testing.T) {
 	assert.True(t, options.Uppercase)
 	assert.True(t, options.Numbers)
 	assert.False(t, options.Symbols)
-	assert.Empty(t, options.Exclude)
-	assert.False(t, options.ExcludeSimilar)
 	assert.Empty(t, options.Custom)
 	assert.Equal(t, 1, options.AvoidRepeats)
 	assert.False(t, options.QrCode)
+}
+
+func TestValidate(t *testing.T) {
+
+	tests := []struct {
+		name           string
+		options        PasswordGeneratorOptions
+		expectedResult bool
+		ErrWant        error
+	}{
+		{
+			name: "valid length",
+			options: PasswordGeneratorOptions{
+				Length: 10,
+			},
+			expectedResult: true,
+			ErrWant:        nil,
+		},
+		{
+			name: "zero length",
+			options: PasswordGeneratorOptions{
+				Length: 0,
+			},
+			expectedResult: false,
+			ErrWant:        ErrLengthMustBeGreaterThanZero,
+		},
+		{
+			name: "negative length",
+			options: PasswordGeneratorOptions{
+				Length: -33,
+			},
+			expectedResult: false,
+			ErrWant:        ErrLengthMustBeGreaterThanZero,
+		},
+		{
+			name: "valid avoid repeats",
+			options: PasswordGeneratorOptions{
+				Length:       10,
+				AvoidRepeats: 2,
+			},
+			expectedResult: true,
+			ErrWant:        nil,
+		},
+		{
+			name: "zero avoid repeats",
+			options: PasswordGeneratorOptions{
+				Length:       8,
+				AvoidRepeats: 0,
+			},
+			expectedResult: true,
+			ErrWant:        nil,
+		},
+		{
+			name: "negative avoid repeats",
+			options: PasswordGeneratorOptions{
+				Length:       8,
+				AvoidRepeats: -10,
+			},
+			expectedResult: false,
+			ErrWant:        ErrAvoidRepeatsMustBeEqualOrGreaterThanZero,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			options := NewPasswordGeneratorOptions()
+			options.Length = tt.options.Length
+			options.AvoidRepeats = tt.options.AvoidRepeats
+			result, err := options.Validate()
+
+			assert.Equal(t, tt.expectedResult, result)
+			if tt.ErrWant != nil {
+				assert.Error(t, err)
+				assert.Equal(t, tt.ErrWant, err)
+			} else {
+				assert.NoError(t, err)
+			}
+		})
+	}
 }
